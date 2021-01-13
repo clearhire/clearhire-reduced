68ef6d4fa8fcad8e8698fe65d531a8e9329b7b8d
jasminerienecker99 <33223308+jasminerienecker99@users.noreply.github.com>
2021-01-13 11:36:58 +0000

    adding server

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ external_stylesheets = ['https:codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(style={'backgroundColor': '#3c73a8'}, children=[
     html.Div(
         id='header', 
