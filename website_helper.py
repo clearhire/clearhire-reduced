852d5d52ace3c10c8b61a24f00efad6b5638434f
jasminerienecker99 <33223308+jasminerienecker99@users.noreply.github.com>
2021-01-13 14:01:12 +0000

    Update website_helper.py

--- website_helper.py
+++ website_helper.py
@@ -19,31 +19,31 @@ def generate_table_without_explanations(df):
                 ], 
                 data=recommendations.to_dict('records'),
                 style_data={
-                    'whiteSpace': 'normal',
+                    'whiteSpace': 'nowrap',
                 },
-                css=[{
-                    'selector': '.dash-spreadsheet td div',
-                    'rule': '''
-                        line-height: 15px;
-                        max-height: 90px; min-height: 90px; height: 90px;
-                        display: block;
-                        overflow-y: hidden;
-                    '''
-                }],
-                tooltip_data=[
-                    {
-                        column: {'value': str(value)}
-                        for column, value in row.items()
-                    } for row in recommendations.to_dict('rows')
-                ],
-                tooltip_duration=None,
                 style_cell={
                     'textAlign': 'left',
                     'font-family': 'Arial',
                     'fontSize': 11,
                     'backgroundColor': '#d9ecf3',
-                    'padding': '5px'
+                    'padding': '5px', 
+                    'textOverflow': 'ellipsis',
+                    'overflow': 'hidden',
+                    'maxWidth': '175px', 
+                    'minWidth': '175px',
+                    'width': '175px',
+                    'minHeight': '45px', 
+                    'height': '45px', 
                 },
+                style_cell_conditional=[{
+                    'if': {
+                        'column_id': 'Title',
+                    },
+                        'minWidth': '270px',
+                        'maxWidth': '270px',
+                        'width': '270px',
+                        'fontWeight': 'bold'
+                }],
                 style_header={
                     'fontWeight': 'bold',
                     'backgroundColor': '#a7dff3e8',
@@ -75,40 +75,41 @@ def generate_table(df):
                     {"name": 'Explanations', "id": 'Explanations'}
                 ], 
                 data=recommendations.to_dict('records'),
-                style_data={
-                    'whiteSpace': 'normal',
-                },
-                style_data_conditional=[{
-                    'if': {
-                        'column_id': 'Explanations',
-                    },
-                    'minWidth': '255px',
-                    'maxWidth': '255px',
-                    'width': '255px',
-                    'fontWeight': 'bold'
-                }],
-                css=[{
-                    'selector': '.dash-spreadsheet td div',
-                    'rule': '''
-                        line-height: 15px;
-                        max-height: 90px; min-height: 90px; height: 90px;
-                        display: block;
-                        overflow-y: hidden;
-                    '''
-                }],
-                tooltip_data=[
+                style_cell_conditional=[
                     {
-                        column: {'value': str(value)}
-                        for column, value in row.items()
-                    } for row in recommendations.to_dict('rows')
+                        'if': {
+                            'column_id': 'Explanations',
+                        },
+                        'minWidth': '235px',
+                        'maxWidth': '235px',
+                        'width': '235px',
+                        'fontWeight': 'bold',
+                        'whiteSpace': 'normal'
+                    },
+                    {
+                        'if': {
+                            'column_id': 'Title',
+                        },
+                        'minWidth': '205px',
+                        'maxWidth': '205px',
+                        'width': '205px',
+                        'fontWeight': 'bold',
+                    },
                 ],
-                tooltip_duration=None,
                 style_cell={
                     'textAlign': 'left',
                     'font-family': 'Arial',
                     'fontSize': 11,
                     'backgroundColor': '#d9ecf3',
-                    'padding': '5px'
+                    'padding': '5px', 
+                    'textOverflow': 'ellipsis',
+                    'overflow': 'hidden',
+                    'maxWidth': '150px', 
+                    'minWidth': '150px',
+                    'width': '150px',
+                    'minHeight': '45px', 
+                    'maxHeight': '45px', 
+                    'height': '45px', 
                 },
                 style_header={
                     'fontWeight': 'bold',
@@ -124,4 +125,5 @@ def generate_table(df):
                     'width': '96%'
                 }
             ))
+
 
